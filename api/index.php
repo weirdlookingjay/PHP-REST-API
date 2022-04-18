46ce3f537353c8753b636811f108c283af5b75f5
Joe Clark <weirdlookingjay@live.com>
2022-04-18 13:23:06 -0400

    Added a TaskController class and updated index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
-echo "Index Page";
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$parts = explode("/", $path);
+$resource = $parts[2];
+$id = $parts[3] ?? null;
+
+if ($resource != "tasks") {
+    http_response_code(404);
+    exit;
+}
+
+require dirname(__DIR__) . "/src/TaskController.php";
+$controller = new TaskController;
+$controller->processRequest($_SERVER["REQUEST_METHOD"], $id);
+
