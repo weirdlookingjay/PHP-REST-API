c305451b802879e484d4273cd4986dedf10d5395
Joe Clark <weirdlookingjay@live.com>
2022-04-18 15:29:43 -0400

    Fetch all tasks and Convert database Booleans

--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -6,4 +6,17 @@ class TaskGateway
     public function __construct(Database $database) {
         $this->conn = $database->getConnection();
     }
+
+    public function getAll(): array {
+        $sql = "SELECT * FROM task ORDER BY name";
+
+        $stmt = $this->conn->query($sql);
+        $data = [];
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $row['is_completed'] = (bool) $row['is_completed'];
+            $data[] = $row;
+        }
+        return $data;
+    }
 }
