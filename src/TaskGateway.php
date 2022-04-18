e055653a47893e60c8374cfbb830c7d0e7ac0d2c
Joe Clark <weirdlookingjay@live.com>
2022-04-18 15:57:35 -0400

    Show individual records

--- src/TaskGateway.php
+++ src/TaskGateway.php
@@ -19,4 +19,19 @@ class TaskGateway
         }
         return $data;
     }
+
+    public function get(string $id): array | false {
+        $sql = "SELECT * FROM task WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($data !== false) {
+            $data['is_completed'] = (bool) $data['is_completed'];
+        }
+
+        return $data;
+    }
 }
